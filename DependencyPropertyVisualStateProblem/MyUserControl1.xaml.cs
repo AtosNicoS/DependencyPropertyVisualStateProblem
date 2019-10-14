0172f16542a0bbd100a89df2c868323da4e1fa1d
nico.schulz <nico.schulz@atos.net>
2019-10-14 14:44:08 +0200

    Initial upload

---
+++ DependencyPropertyVisualStateProblem/MyUserControl1.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace DependencyPropertyVisualStateProblem
+{
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
+    public sealed partial class MyUserControl1 : UserControl
+    {
+        public MyUserControl1()
+        {
+            this.InitializeComponent();
+        }
+
+        public static readonly DependencyProperty IsButtonCheckedProperty =
+            DependencyProperty.Register(nameof(IsButtonChecked), typeof(bool),
+                typeof(MyUserControl1), new PropertyMetadata(null));
+
+        public bool IsButtonChecked
+        {
+            get => (bool)GetValue(IsButtonCheckedProperty);
+            set => SetValue(IsButtonCheckedProperty, value);
+        }
+    }
+}
